0112f7179c3ba985f58da8e846c437c7da634593
Alexandru-Cristian Pop <alexandru.pop98@yahoo.com>
2020-11-23 21:18:24 +0200

    Succeded to animate george's movement, but broke mario's one

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -10,17 +10,66 @@ export class Player {
         this.x = x;
         this.y = y;
         this.step = step;
+        this.sprites = this.getSprites("../../assets/sprites/george.png", 4, 4, { width: 10, height: 10 }, { width: [0, 17, 22, 20], height: [0, 17, 16, 16] });
+        this.currentSprite = {
+            sprite: this.sprites.down[0],
+            direction: "down",
+            index: 0,
+        }
 
         this.topLimit = step;
         this.botLimit = canvas.height - height - step;
         this.leftLimit = step;
         this.rightLimit = canvas.width - width - step;
     }
-    draw(context) {
+    getSprites(path, rows, columns, offsetBefore, offsetBetween) {
+        let sprites = {
+            up: [],
+            down: [],
+            left: [],
+            right: [],
+        };
+
+        for (let i = 0; i < rows; i++)
+            for (let j = 0; j < columns; j++) {
+                let sprite = {
+                    x: offsetBefore.width + (this.width + offsetBetween.width[j]) * j,
+                    y: offsetBefore.height + (this.height + offsetBetween.height[i]) * i,
+                };
+
+                switch (j) {
+                    case 0: { sprites.down.push(sprite); break; }
+                    case 1: { sprites.left.push(sprite); break; }
+                    case 2: { sprites.up.push(sprite); break; }
+                    case 3: { sprites.right.push(sprite); break; }
+                }
+            }
+        return sprites;
+    }
+    getNextSprite(direction){
+        if (this.currentSprite.direction == direction){
+            this.currentSprite.index++;
+            this.currentSprite.index = this.currentSprite.index % 4;
+        }
+        else{
+            this.currentSprite.index = 0;
+            this.currentSprite.direction = direction;
+        }
+
+        switch(this.currentSprite.direction){
+            case "up": {this.currentSprite.sprite = this.sprites.up[this.currentSprite.index]; break;};
+            case "down": {this.currentSprite.sprite = this.sprites.down[this.currentSprite.index]; break;};
+            case "left": {this.currentSprite.sprite = this.sprites.left[this.currentSprite.index]; break;};
+            case "right": {this.currentSprite.sprite = this.sprites.right[this.currentSprite.index]; break;};
+        }
+    }
+
+    draw(context, direction) {
+        this.getNextSprite(direction);
         context.drawImage(
             this.image,
-            0 * this.width,
-            0 * this.height,
+            this.currentSprite.sprite.x,
+            this.currentSprite.sprite.y,
             this.width,
             this.height,
             this.x,
--- js/scripts/main.js
+++ js/scripts/main.js
@@ -14,34 +14,33 @@ let backgroundImage = new Image();
 backgroundImage.src = "../../assets/sprites/desert.png";
 
 let mario = new Player("Mario", "../../assets/sprites/mario.png", 32, 39, 0, 0, canvas, STEP);
-let george = new Player("George", "../../assets/sprites/george.png", 40, 45, 100, 100, canvas, STEP);
+let george = new Player("George", "../../assets/sprites/george.png", 29, 32, 100, 100, canvas, STEP);
 
 const clearCanvas = () => {
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.drawImage(backgroundImage,0,0);
 }
 
-const redrawPlayers = () => {
+const redrawPlayers = (direction) => {
     clearCanvas();
-    george.draw(context);
-    mario.draw(context);
+    george.draw(context, direction);
+    mario.draw(context, direction);
 }
 
 window.onload = () => {
-    redrawPlayers();
+    redrawPlayers("down");
 };
 
 document.addEventListener("keydown", (event) => {
     switch (event.key) {
-        case "ArrowUp": { validator.validateUp(george); break; }
-        case "ArrowDown": { validator.validateDown(george); break; }
-        case "ArrowLeft": { validator.validateLeft(george); break; }
-        case "ArrowRight": { validator.validateRight(george); break; }
+        case "ArrowUp": { validator.validateUp(george); redrawPlayers("up"); break; }
+        case "ArrowDown": { validator.validateDown(george); redrawPlayers("down");  break; }
+        case "ArrowLeft": { validator.validateLeft(george); redrawPlayers("left");  break; }
+        case "ArrowRight": { validator.validateRight(george); redrawPlayers("right");  break; }
 
-        case "w": { validator.validateUp(mario); break; }
-        case "s": { validator.validateDown(mario); break; }
-        case "a": { validator.validateLeft(mario); break; }
-        case "d": { validator.validateRight(mario); break; }
+        case "w": { validator.validateUp(mario); redrawPlayers("up");  break; }
+        case "s": { validator.validateDown(mario); redrawPlayers("down");  break; }
+        case "a": { validator.validateLeft(mario); redrawPlayers("left");  break; }
+        case "d": { validator.validateRight(mario); redrawPlayers("right");  break; }
     }
-    redrawPlayers();
 });
